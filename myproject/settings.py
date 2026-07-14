dcc68023827caa46e0d5fb7ae8d5c4ab95f97e3f
Suyog Chavan <chavansuyog2005@gmail.com>
2026-07-14 16:37:14 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -171,17 +171,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 
-# Email Configuration
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
-EMAIL_TIMEOUT = 10
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
